b67b79cf79bd5beb219184cb9dbc9f9754d82f7e
CEM <cem@karimzoda.local>
2026-02-12 16:09:42 +0100

    Fix abg and code  improvement

--- homework-5.js
+++ homework-5.js
@@ -1,36 +1,60 @@
-// Функи которая выводит город и градус
-function cityTemperature() {
-    const city = "Варшава";
-    const temperature = 3;
+// (Homework-5) (Задание №1) Пройден тест отправлен в репозиторий
 
+// (Homework-5) (Задание №2) Создан файо homework-5.js добавлен в html
+
+// (Homework-5) (Задание №3): Функи которая выводит город и градус
+const city = "Варшава";
+const temperature = 3;
+
+function printCityTemperature(city, temperature) {
     console.log("Сейчас в " +  city  +  " температура " +  temperature + " градусов по Цельсию"); 
 };
-cityTemperature();
+printCityTemperature(city, temperature);
 
-// Функция котрая сравнивает скорость света с задданым аргументом.    
-const speedOfLight = 299792458;
+// (Homework-5) (Задание №4): Функция котрая сравнивает скорость света с задданым аргументом.    
+const SPEED_OF_LIGHT = 299792458;
 
-function checkingLight(light) {
-    if (light > speedOfLight) {
+function checkLightSpeed(light) {
+    if (light > SPEED_OF_LIGHT) {
         console.log("Сверхсветовая скорость");
-    } else if (light < speedOfLight) {
+    } else if (light < SPEED_OF_LIGHT) {
         console.log("Субсветовая  скорость");
     } else {
         console.log("Скорость света");
     };
 };
-checkingLight(299792459);
+checkLightSpeed(299792459);
 
-// Функция которая сравнивает бюджет и лимит. 
+// (Homework-5) (Задание №5): Функция которая сравнивает бюджет и лимит. 
 const accessory = "Айфон 15"; 
 const price = 3500; 
 
-function myLimit(limit) {
-    if (limit >= price) {
+function myBudget(budget) {
+    if (budget >= price) {
         console.log(accessory + " приобретен. Спасибо за покупку!");
     } else {
-        const differnce = price - limit
-        console.log(`Вам не хватает ${differnce} пополните баланс.`);
+        const difference = price - budget
+        console.log(`Вам не хватает ${difference} пополните баланс.`);
     };
 };
-myLimit(3100);
+myBudget(3100);
+
+// (Homework-5) (Задание №6) Создать функцию и именовать на свое усмотрение 
+function verifyAge(ageVerification) {
+    const age = 18;
+    if (ageVerification >= age) {
+        console.log("Добро пожаловать");
+    } else  {
+        console.log("Вы являетесь несовершеннолетним");
+    };      
+};
+verifyAge(18);
+
+// (Homework-5) (Задание №7) Создать 3 переменных 
+const fullName = "Mumin Suleimanov";
+const countryAndCity = "Poland, Warszawa";
+const address = "Aleja kwakowska 82";
+
+console.log(fullName);
+console.log(countryAndCity);
+console.log(address);
